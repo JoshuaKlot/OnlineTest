d357252b7d85b4c4cb939fb6d218fc300938d373
JoshuaKlot <107051800+JoshuaKlot@users.noreply.github.com>
2025-04-17 22:35:38 -0400

    Added Cursor

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    [SerializeField] private GameObject coins;
+    [SerializeField] private int numOfCoins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
