32fd44f6914239d8e2dac25d6ea3a9b92811400d
JoshuaKlot <joshklotzkin@gmail.com>
2025-05-28 12:50:19 -0400

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -56,7 +56,7 @@ public class GameManager : NetworkBehaviour
         if (AllPlayersReady() && !playersSpawned)
         {
             playersSpawned = true;
-            RevealCoinsToOtherPlayers();
+            //RevealCoinsToOtherPlayers();
             SpawnAllPlayerB();
         }
 
