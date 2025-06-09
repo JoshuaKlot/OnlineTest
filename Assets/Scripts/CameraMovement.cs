769b780523585e8aa7e3103a1a5c459ab1220f92
JoshuaKlot <joshklotzkin@gmail.com>
2025-06-09 14:58:11 -0400

    attemptedCinemachine

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class CameraMovement : NetworkBehaviour
+{
+    [SerializeField] private float vSpeed;
+    [SerializeField] private float hSpeed;
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        if (!IsOwner) return; // Only allow local player to move their character
+
+        float hMovemnent = Input.GetAxisRaw("Horizontal");
+        float vMovement = Input.GetAxisRaw("Vertical");
+        rb2d.linearVelocity = new Vector2(hMovemnent * hSpeed, vSpeed * vMovement);
+    }
+
+}
